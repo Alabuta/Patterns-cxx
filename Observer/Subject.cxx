c9c4524de2d33faa3ad2b2b899e7e0ad79fbf94a
Almaz Kinziabulatov <auqolaq@gmail.com>
2017-02-26 20:27:46 +0300

    Fixing potential problems with weak pointers.

--- Observer/Subject.cxx
+++ Observer/Subject.cxx
@@ -5,26 +5,38 @@
 #include "Subject.h"
 
 
-void Subject::AddObserver(std::shared_ptr<IObserver> const &observer)
+void Subject::AddObserver(std::shared_ptr<IObserver> const &_observer)
 {
-    auto duplicate = std::find_if(observers_.begin(), observers_.end(), [&observer = observer] (auto const &p)
+    for (auto &observer : observers_)
+        if (observer.expired())
+            observer.reset();
+
+    auto duplicate = std::find_if(observers_.begin(), observers_.end(), [&observer = _observer] (auto const &p)
     {
-        if (!p.expired())
-            return observer == p.lock();
+        auto locked = p.lock();
+
+        if (locked)
+            return observer == locked;
 
         return false;
     }) != observers_.cend();
 
     if (!duplicate)
-        observers_.push_front(observer);
+        observers_.push_front(_observer);
 }
 
-void Subject::RemoveObserver(std::shared_ptr<IObserver> const &observer)
+void Subject::RemoveObserver(std::shared_ptr<IObserver> const &_observer)
 {
-    observers_.remove_if([&observer = observer] (auto const &p)
+    for (auto &observer : observers_)
+        if (observer.expired())
+            observer.reset();
+
+    observers_.remove_if([&observer = _observer] (auto const &p)
     {
-        if (!p.expired())
-            return observer == p.lock();
+        auto locked = p.lock();
+
+        if (locked)
+            return observer == locked;
 
         return false;
     });
@@ -42,9 +54,14 @@ void Subject::NotifyObservers() const
         value(mt)
     };
 
-    for (auto const &observer : observers_)
-        if (!observer.expired())
-            observer.lock()->HandleEvent(e);
+    std::shared_ptr<IObserver> temp;
+
+    for (auto const &observer : observers_) {
+        temp = observer.lock();
+
+        if (temp)
+            temp->HandleEvent(e);
+    }
 
     std::cout << std::endl;
 }
