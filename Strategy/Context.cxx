b4179212227a22d6597aac2a7967fc630db63b02
Almaz Kinziabulatov <auqolaq@gmail.com>
2017-05-14 15:37:04 +0300

    Minor changes.

--- Strategy/Context.cxx
+++ Strategy/Context.cxx
@@ -4,9 +4,10 @@
 Context::Context() : strategy_(nullptr)
 {}
 
-void Context::SetStrategy(std::unique_ptr<IStrategy> &&strategy)
+void Context::SetStrategy(std::unique_ptr<IStrategy> &&_strategy)
 {
-    strategy_ = std::move(strategy);
+    if (_strategy)
+        strategy_ = std::move(_strategy);
 }
 
 void Context::UseStrategy()
--- Strategy/Context.h
+++ Strategy/Context.h
@@ -1,5 +1,6 @@
 #pragma once
-#include "memory"
+#include <memory>
+
 #include "IStrategy.h"
 
 class Context final {
